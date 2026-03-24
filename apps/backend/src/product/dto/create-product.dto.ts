918576ecd6383e4db0289aed233bef08edb95daa
erzan <do.earljan@gmail.com>
2026-03-25 06:22:34 +0800

    added api property on creating products

--- apps/backend/src/product/dto/create-product.dto.ts
+++ apps/backend/src/product/dto/create-product.dto.ts
@@ -1,24 +1,49 @@
-import { IsString, IsOptional, IsNumber, Min, IsPositive, IsInt } from 'class-validator'
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, IsNumber, Min, IsPositive, IsInt, IsNotEmpty, isNotEmpty } from 'class-validator'
 
 export class CreateProductDto {
   @IsString()
-  name: string;
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 'Iphone',
+    description: 'Name of the product'
+  })
+  name!: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    example: 'Brand new iphone',
+    description: 'Description of the product'
+  })
   description?: string;
 
   @IsInt()
   @IsNumber()
   @IsPositive()
-  price: number;
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 19000,
+    description: 'Price of the product'
+  })
+  price!: number;
 
   @IsInt()
   @IsNumber()
   @Min(0)
-  quantity: number;
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 2,
+    description: 'Quantiy/No of amount of the product'
+  })
+  quantity!: number;
 
   @IsInt()
   @IsNumber()
-  categoryId: number; // required for connect
+  @IsNotEmpty()
+  @ApiProperty({
+    example: 2,
+    description: 'The category ID the product belongs'
+  })
+  categoryId!: number; // required for connect
 }
