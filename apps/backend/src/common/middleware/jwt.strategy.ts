13ff72dd99f21a51452fdc0b2e793ee176d374fb
erzan <do.earljan@gmail.com>
2026-03-28 21:05:00 +0800

    adjusted jwt strategy to check for cookies instead of auth bearer tokn

--- apps/backend/src/common/middleware/jwt.strategy.ts
+++ apps/backend/src/common/middleware/jwt.strategy.ts
@@ -2,26 +2,52 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from '../../shared/types/interface';
+import { RequestUser } from '../../shared/types/request-user.interface';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Request } from 'express';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor(configService: ConfigService) {
-        const jwtSecret = configService.get<string>('JWT_SECRET');
-            if (!jwtSecret) {
-            throw new Error('JWT_SECRET is not defined in the environment variables');
-            }
-
+    constructor(configService: ConfigService, private readonly prisma: PrismaService) {
+        const secret = process.env.JWT_SECRET;
+        if (!secret) {
+            throw new Error('JWT_SECRET environment variable is not defined');
+        }
         super({
-            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: jwtSecret,
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                (request: Request): string | null => {
+                return typeof request.cookies?.['accessToken'] === 'string'
+                    ? request.cookies['accessToken']
+                    : null;
+                },
+            ]),
+
+            secretOrKey: secret,
         });
     }
 
-    async validate(payload: any) {
+    async validate(payload: JwtPayload): Promise<RequestUser> {
+        const user = await this.prisma.user.findUnique({
+        where: { id: payload.id },
+            include: {
+                orders: true,
+                stores: true,
+                cartItem: true, 
+                userProfile: true,
+            }
+        });
+
+        if (!user) {
+            throw new Error('User not found'); // or UnauthorizedException for JWT guard
+        }
+
         return { 
-            userId: payload.sub, 
-            email: payload.email,
-            role: payload.role 
+            id: user.id,
+            email: user.email,
+            role: user.role,
+            token_version: user.token_version,
+            is_active: user.is_active,
         };
     }
 }
