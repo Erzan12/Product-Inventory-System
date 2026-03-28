044668683cb69d3b21280ed2382b8e3a671f2294
erzan <do.earljan@gmail.com>
2026-03-28 18:33:57 +0800

    backend - updated order dto and user dto

---
+++ apps/backend/src/modules/order/dto/order.dto.ts
@@ -0,0 +1,39 @@
+import { IsArray, IsEnum, IsInt, IsNotEmpty, IsNumber, IsPositive, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { OrderStatus } from '../../../shared/constants/enums.decorator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class OrderItemDto {
+  @IsInt()
+  productId!: number;
+
+  @IsInt()
+  @IsPositive()
+  quantity!: number;
+
+  @IsNumber()
+  @IsPositive()
+  price!: number;
+}
+
+export class CreateOrderDto {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items!: OrderItemDto[];
+}
+
+export class UpdateOrderStatusDto {
+    @IsString()
+    @IsNotEmpty()
+    @IsEnum(OrderStatus, {
+        message: 'Order status must be PENDING, PAID, SHIPPED,COMPLETED, CANCELLED'
+    })
+    @Type(() => String)
+    @ApiProperty({
+        enum: OrderStatus,
+        example: OrderStatus.PENDING,
+        description: 'The order status of this product'
+    })
+    status!: OrderStatus;
+}
